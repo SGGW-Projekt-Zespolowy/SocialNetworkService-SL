d6a9b6abbc4d7c722b83b5a491a4905ce12de7e4
JWierzbix <j.wierzbicki2001@gmail.com>
2023-03-24 23:55:40 +0100

    Revert "edited comment - test"
    This reverts commit d3c18bfd1958895ba3f91d4bfd859af7fd35d80c.

--- SocialNetworkService/SocialNetworkService/Program.cs
+++ SocialNetworkService/SocialNetworkService/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-// edytowano testowy komentarz
+// dodano testowy komentarz
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
