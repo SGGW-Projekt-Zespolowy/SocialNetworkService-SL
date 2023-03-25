9d416b70ce753286a1f3f6268bf8372364713a02
JWierzbix <j.wierzbicki2001@gmail.com>
2023-03-25 01:09:48 +0100

    Added Clean Architecture Structure

--- SocialNetworkService/SocialNetworkService/Program.cs
+++ SocialNetworkService/SocialNetworkService/Program.cs
@@ -1,16 +1,29 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
