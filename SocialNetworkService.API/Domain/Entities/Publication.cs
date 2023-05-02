0fec20b96a7556def39684eabbf8fd9d876cea22
JWierzbix <j.wierzbicki2001@gmail.com>
2023-05-02 23:23:15 +0200

    Added Publications

---
+++ SocialNetworkService.API/Domain/Entities/Publication.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities
+{
+    public class Publication: Entity
+    {
+        public Publication(Guid id, Guid authorId, string title, string content, string link, string picture, Specialization type)
+            :base(id)
+        {
+            AuthorId = authorId;
+            Title = title;
+            Content = content;
+            Link = link;
+            Picture = picture;
+            Type = type;
+        }
+
+        public Guid AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Link { get; set; }
+        public string Picture { get; set; }
+        public Specialization Type { get; set; }
+    }
+}
