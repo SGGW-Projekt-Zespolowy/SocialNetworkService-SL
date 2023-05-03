f4dfa60819b16ee9407d8d5291b68cd6a2ce9e90
JWierzbix <j.wierzbicki2001@gmail.com>
2023-05-03 23:25:22 +0200

    updated User class

--- SocialNetworkService.API/Domain/Entities/User.cs
+++ SocialNetworkService.API/Domain/Entities/User.cs
@@ -5,7 +5,7 @@ namespace Domain.Entities
 {
     public sealed class User : Entity
     {
-        public User(Guid id, string email, DateTime lastLoginDate,
+        public User(Guid id, Email email, DateTime lastLoginDate,
             DateTime registrationDate, FirstName firstName, LastName lastName,
             DateTime dateOfBirth, Degree degree) :
             base(id)
@@ -18,7 +18,7 @@ namespace Domain.Entities
             DateOfBirth = dateOfBirth;
             Degree = degree;
         }
-        public string Email { get; set; }
+        public Email Email { get; set; }
         public DateTime LastLoginDate { get; set; }
         public DateTime RegistrationDate { get; set; }
         public FirstName FirstName { get; set; }
