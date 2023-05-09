6c56274c52eda2a9beb421e5222a170f549c8528
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-05-10 00:07:45 +0200

    Added other ValueObjects and their basic validation

---
+++ SocialNetworkService.API/Domain/ValueObjects/Content.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Primitives;
+using Domain.Shared;
+
+namespace Domain.ValueObjects
+{
+    public class Content : ValueObject
+    {
+        public const int MaxContentLength = 30000;
+        private Content(string value) 
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+
+        public Result<Content> Create(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return Result.Failure<Content>(new Error(
+                        "Content.Empty",
+                        "Content is empty."));
+
+            if (content.Length > MaxContentLength)
+                return Result.Failure<Content>(new Error(
+                    "Content.TooLong",
+                    "Content is too long."));
+
+            return new Content(content);
+        }
+    }
+}
---
+++ SocialNetworkService.API/Domain/ValueObjects/Degree.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Domain.Primitives;
+using Domain.Shared;
+
+namespace Domain.ValueObjects
+{
+    public class Degree : ValueObject
+    {
+        public const int MaxDegreeLength = 50;
+        private Degree(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+        public Result<Degree> Create(string degree)
+        {
+            if (string.IsNullOrEmpty(degree))
+                return Result.Failure<Degree>(new Error(
+                    "Degree.Empty",
+                    "Degree of user is empty."));
+
+            if (degree.Length > MaxDegreeLength)
+                return Result.Failure<Degree>(new Error(
+                    "Degree.TooLongName",
+                    "Degree of user is too long."));
+
+            if (!Enum.IsDefined(typeof(DegreeEnum), degree))
+                return Result.Failure<Degree>(new Error(
+                    "Degree.NotDefined",
+                    "Degree of user is not defined."));
+
+            return new Degree(degree);
+        }
+    }
+}
